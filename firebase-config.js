064c2f62cef38308ced8094272bf060056cecd46
Carlesneri <carlesneri@hotmail.com>
2021-10-07 19:39:16 +0200

    fix: removing files

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,14 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDb56-pbdkjupDoiD5ooKRD0JpvJRrCgFQ",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "neritech-57f2d.firebaseapp.com",
     projectId: "neritech-57f2d",
     storageBucket: "neritech-57f2d.appspot.com",
     messagingSenderId: "737271616573",
-    appId: "1:737271616573:web:cafbe9ac11c04836354654",
+    appId: process.env.FIREBASE_APP_ID,
     measurementId: "G-TQQ54VMSRY"
 };
 
